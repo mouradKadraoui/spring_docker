048888e4bc9e046ab2a28c245912183bb6b7beb8
mouradKadraoui <mkadraoui46@gmail.com>
2024-06-05 10:07:42 +0100

    add controller

--- src/main/java/com/example/spring_docker/SpringDockerApplication.java
+++ src/main/java/com/example/spring_docker/SpringDockerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping
+@RequestMapping("/")
 public class SpringDockerApplication {
 
 	@GetMapping
