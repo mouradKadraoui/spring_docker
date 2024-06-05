ab642c7cdf3612ad89c0c8bf8859190a3210f6e9
mouradKadraoui <mkadraoui46@gmail.com>
2024-06-05 09:44:45 +0100

    Initial commit

---
+++ src/main/java/com/example/spring_docker/SpringDockerApplication.java
@@ -0,0 +1,23 @@
+package com.example.spring_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+public class SpringDockerApplication {
+
+	@GetMapping
+	public String get(){
+		return "Mourad KADRAOUI";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerApplication.class, args);
+	}
+
+}
